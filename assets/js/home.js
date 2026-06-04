6fd2af363312cfc2b5d644db56784b98d2ac9f32
ankana3 <ankana.desing@gmail.com>
2026-06-03 23:03:26 -0500

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,32 @@
+const track = document.querySelector('.carousel-track');
+const container = document.querySelector('.carousel-container');
 
+let speed = 0.4;
+let position = 0;
+let animation;
+
+function animateCarousel() {
+
+    position -= speed;
+
+    const halfWidth = track.scrollWidth / 2;
+
+    if (Math.abs(position) >= halfWidth) {
+        position = 0;
+    }
+
+    track.style.transform = `translateX(${position}px)`;
+
+    animation = requestAnimationFrame(animateCarousel);
+}
+
+animateCarousel();
+
+
+container.addEventListener('mouseenter', () => {
+    cancelAnimationFrame(animation);
+});
+
+container.addEventListener('mouseleave', () => {
+    animateCarousel();
+});
