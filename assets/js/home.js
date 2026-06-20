e6da3427fc86212e667da2172a59ea82fd977cc6
ankana3 <ankana.desing@gmail.com>
2026-06-20 15:52:41 -0500

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -11,7 +11,7 @@ function updateCarousel() {
 
     items.forEach((item, index) => {
 
-        const angle = const angle = ((360 / total) * index) + rotation - 10;
+        const angle = ((360 / total) * index) + rotation - 10;
 
         const rad = angle * Math.PI / 180;
 
